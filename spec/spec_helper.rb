1e636574fb91ca923c733064af46a4faa8c2063b
sue445 <sue445@sue445.net>
2014-03-02 23:44:49 +0900

    Tweak

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'coveralls'
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'google_holiday_calendar'
 
@@ -6,9 +9,6 @@ require 'rspec/its'
 require 'rspec/collection_matchers'
 require 'delorean'
 require 'memoist'
-
-require 'coveralls'
-Coveralls.wear!
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
