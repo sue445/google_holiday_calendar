ff58ba118c1ad1b5023d0561561c2b91573f87f3
sue445 <sue445@sue445.net>
2016-11-25 01:32:43 +0900

    Fix failed spec

--- spec/support/examples/holidays_in_usa_at_jan.rb
+++ spec/support/examples/holidays_in_usa_at_jan.rb
@@ -1,5 +1,6 @@
 shared_examples :holidays_in_usa_at_jan do
-  its(:count){ should == 2 }
-  it{ should include(date("2014-01-01") => "New Year's Day") }
-  it{ should include(date("2014-01-20") => "Martin Luther King Day") }
+  its(:count){ should == 3 }
+  it{ should include(date("2017-01-01") => "New Year's Day") }
+  it{ should include(date("2017-01-02") => "New Year's Day observed") }
+  it{ should include(date("2017-01-16") => "Martin Luther King Day") }
 end
